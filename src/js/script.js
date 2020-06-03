26ac7c300dcab652392adb258b913503e73a9923
fernando39357 <fernando39357@gmail.com>
2020-06-03 14:00:42 -0300

    Ajustes

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,11 @@ document.addEventListener("readystatechange", function () {
 		valorCalculado.style.display = "none";
 
 		localStorage.getItem("valorFrete")
-			? (inputFrete.value = localStorage.getItem("valorFrete"))
+			? (inputFrete.value = parseFloat(
+					localStorage.getItem("valorFrete").replace(",", ".")
+			  )
+					.toFixed(2)
+					.replace(".", ","))
 			: (inputFrete.value = "");
 
 		const calcular = (valorDesejado) => {};
@@ -19,22 +23,34 @@ document.addEventListener("readystatechange", function () {
 		};
 
 		inputDesejado.addEventListener("input", () => {
-			valorDesejado = parseFloat(inputDesejado.value);
+			valorDesejado = parseFloat(inputDesejado.value.replace(",", "."));
 			if (inputDesejado.value != "") {
 				if (valorDesejado > 120) {
 					valorDesejado =
-						valorDesejado + 5 + parseFloat(localStorage.getItem("valorFrete"));
+						valorDesejado +
+						5 +
+						parseFloat(localStorage.getItem("valorFrete").replace(",", "."));
 				} else {
 					valorDesejado = valorDesejado + 5;
 				}
 				let calculaClassico = (valorDesejado / 0.89).toFixed(2);
 				let calculaPremium = (valorDesejado / 0.84).toFixed(2);
 
-				classico.innerHTML = "Anúncio Clássico: R$" + calculaClassico;
-				premium.innerHTML = "Anúncio Premium: R$" + calculaPremium;
+				classico.innerHTML =
+					"Anúncio Clássico: R$" + calculaClassico.replace(".", ",");
+				premium.innerHTML =
+					"Anúncio Premium: R$" + calculaPremium.replace(".", ",");
+
+				valorCalculado.style.opacity = 0;
 				valorCalculado.style.display = "flex";
+				setTimeout(() => {
+					valorCalculado.style.opacity = 1;
+				}, 500);
 			} else {
-				valorCalculado.style.display = "none";
+				valorCalculado.style.opacity = 0;
+				setTimeout(() => {
+					valorCalculado.style.display = "none";
+				}, 500);
 			}
 		});
 		inputFrete.addEventListener("input", salvarFrete);
